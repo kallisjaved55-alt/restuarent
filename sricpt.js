29e758a369effe76ad601afa58a5fff5e08c6d93
Kallis Javed <kallisjaved55@gmail.com>
2026-04-22 10:47:43 +0500

    first commit

---
+++ sricpt.js
@@ -0,0 +1,27 @@
+// let name = "ali";
+// document.write("student Name: "+Name);
+
+// let fruits = ["apple","banana","mango"];
+// document.write(fruits[2]);
+
+// let age = 15;
+// document.write("number:", age);
+
+// let isStudent = false;
+// document.write("boolean:",isStudent,);
+
+// let city ;
+// document.write("undefined:",city);
+
+// let emptyvalue = null;
+// document.write("null :",emptyvalue);
+
+// let colors = ["red","blue","green"];
+// document.write("array:",colors);
+
+var studentname = "rahul";
+var mark = 85;
+console.log(studentname);
+console.log(mark);
+
+
